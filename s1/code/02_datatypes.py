6c74a505ab778a49ec00c9c0d310e6a41878e667
Prograin <ahmadreza.pcg1377@gmail.com>
2026-07-14 08:39:10 -0700

    init

---
+++ s1/code/02_datatypes.py
@@ -0,0 +1,39 @@
+# در گوشی آیا همه چی عدد هست؟
+# تکست هست؟
+# آیا مقدارهای درست و غلط نداریم
+
+# برای ذخیره مخاطب شما عدد دارید برای شماره شخص
+# متن دارید برای اسم شخص
+# گزینه بلاک کردن دارید برای مشخص کردن بلاک بودن با نبودن شخص
+
+player_name = "Franklin"
+money = 5000
+health = 95.5
+is_alive = True
+
+# Integer
+INTEGER_A = 150
+INTEGER_B = 12312354
+INTEGER_C = -123
+
+print('INTEGER------------------------------')
+print(type(INTEGER_A))
+print('------------------------------')
+
+# Float
+FLOAT_A = 123.25
+FLOAT_B = 451.07
+FLOAT_B = .009
+
+# String
+STRING_A = 'jaklsdfjkasd90-8**(*()@!#@!#@!-09asdkljkljk90-90-90),,,,asiopdjas[][][][][]{}'
+STRING_B = "asjdklas12312   asd"
+STRING_C = "12312354"
+
+# BOOLEAN
+BOOLEAN_A = True
+BOOLEAN_B = False
+
+# SEQUENCE TYPE
+# -LIST-mutable
+a_lt = ["asdasd", 24234, 123213.798, "poorshe", "BENz", -123, 'asdasd']
