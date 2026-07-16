23d1c673af9bc0ca1dcf1ccfea329d4f368c2ce5
samyar <samyarsamyarsamyar139013901390@gmail.com>
2026-07-16 22:00:26 +0330

    تکلیف شماره3

---
+++ s1/samyar_answers/assign_3.py
@@ -0,0 +1,11 @@
+health = 40
+if health < 50:
+    print("Health is low")
+else:
+    print("Health is good")
+
+player_level = 12
+if player_level >= 10:
+    print("You can enter")
+else:
+    print("Level is low")
