b5d6247e586fbb6981061b56cae3b85ff6bcd578
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-07 13:22:34 -0700

    tasks: new tasks are added

---
+++ s1/questions/06_cating_and_before.py
@@ -0,0 +1,33 @@
+# سن یوزر رو بگیر
+# بعدش تایپش رو پرینت کن
+# بعدش نبدیلش کن به int
+# a = input("sen : ")
+# t = type(a)
+# i = int(a)
+# print(t, "_____", i)
+# print(type(i))
+
+# -----------------------------
+
+# b = ['reza', 'ali', 'm,orteza', 'javad', 'maryam', 'nazanin']
+# for x in b:
+#     print(x)
+
+# -----------------------------
+
+# b = ['farahani', 'maryami', 123, 234, 543, 23, 34, 54, 67, 87, 12, 'javadi', 'hoseini']
+# for abc in b:
+#     print(abc)
+
+
+c = [12, 23, 454, 23, 12, 1, 23, 4, 45, 6, 7, 8, 45, 2, 12]
+
+ggg = []
+
+for abc in c:
+    if abc < 10:
+        ggg.append(abc)
+
+        print(ggg)
+
+print(ggg)
