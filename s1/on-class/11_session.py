b6cffe557beba5c5ee5223bc34ccf728fad2cf98
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-21 15:35:04 -0700

    task: new task is added

---
+++ s1/on-class/11_session.py
@@ -0,0 +1,59 @@
+# age_user = input("سن خود را وارد کنید : ")
+# ooo = int(age_user)
+# print(type(ooo))
+
+# if ooo > 20:
+#     print("pir ast.")
+# else:
+#     print("javan ast.")
+
+# correct_pass = 1234
+# correct_username = 'mirza'
+
+# user_pass = input("ramze khod ra vared konid : ")
+# user_name = input("user name khod ra vared konid : ")
+
+# user_pass = int(user_pass)
+# if user_pass == correct_pass and user_name == correct_username:
+#     print("✅")
+# else:
+#     print("try agein !")
+
+# score = 0
+# adad_user = input("adad ra vared konid : ")
+# adad_user = int(adad_user)
+# if adad_user > 20:
+#     score = score + 1
+# else:
+#     score = score - 1
+
+# print(score)
+
+# score = 0
+# "مشت"
+# "مشت قوی"
+# action = input("enter your action : ")
+# if action == "مشت":
+#     score = score + 10
+# elif action == "مشت قوی":
+#     score = score + 20
+# else:
+#     print("مقدار های درست را وارد کنید")
+# print(score)
+
+# names = ["ali", "mohsen", "reza", "hamid"]
+# for ooo in names:
+#     print(ooo)
+# names.append("mirza")
+# print(names)
+
+
+# numbers = [1, 2, 3, 56, 56, 3, 234, 45, 354, 6, 2, 12]
+# numbers_bozorg_tar_az_5 = []
+# for ooo in numbers:
+#     # print(ooo)
+#     if ooo > 5:
+#         numbers_bozorg_tar_az_5.append(ooo)
+
+
+names = ['javad', 'ali']
