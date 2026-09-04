319d483b9b90120481cd31629f102a4180f409de
Prograin <ahmadreza.pcg1377@gmail.com>
2026-09-04 06:05:54 -0700

    task: new tasks are added

---
+++ s1/on-class/15_session.py
@@ -0,0 +1,37 @@
+# empty_list = []
+
+
+# def ooo(a):
+#     if a > 10:
+#        empty_list.append(a)
+
+
+# list_numbers = [12, 23, 34, 1, 23, 2, 6, 8, 34, 6, 8, 45]
+# for ggg in list_numbers:
+#     ooo(ggg)
+
+
+# def func_1():
+#     a = 1
+#     return a
+
+
+# fff = func_1()
+# print(fff)
+
+
+# def func_2(a):
+#     aaa = a * 2
+#     return aaa
+
+
+# b = func_2(4)
+
+
+def ooo(a, b):
+    fff = a * b
+    return fff
+
+
+fff = ooo(3, 7)
+print(type(fff), fff)
